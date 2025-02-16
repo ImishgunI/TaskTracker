87fb11c10888c1c832c5eedfe59da3a4c8c79f50
Michael <mvasilyev04@gmail.com>
2025-02-16 20:59:42 +0300

    implement add command

--- main.go
+++ main.go
@@ -2,18 +2,39 @@ package main
 
 import (
 	"TaskTracker/enums"
-	"fmt"
+	"encoding/json"
 	"log"
 	"os"
+	"time"
 )
+
+type Task struct {
+	Id          int       `json:"id"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
 
 func main() {
 	if len(os.Args) < 2 {
-		log.Fatal("You should use command(add update delete list)\n")
+		log.Fatal("You should use command(add update delete list)")
 	}
+	filename := "tasks.json"
+	var description string
 	command := os.Args[1]
 	result := checkCommand(command)
-	fmt.Print(result)
+	if result == 1 {
+		description = getDescription()
+		addTask(filename, description)
+	}
+}
+
+func getDescription() string {
+	if len(os.Args) < 3 {
+		log.Fatal("You must write a task description")
+	}
+	return os.Args[2]
 }
 
 func checkCommand(command string) int {
@@ -53,4 +74,48 @@ func checkCommandList() int {
 		result = enums.ListAll
 	}
 	return result
+}
+
+func addTask(filename, desc string) error {
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDONLY, 0644)
+	var tasks []Task
+	if err != nil && err.Error() != "EOF" {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	info, err := os.Stat(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if info.Size() > 0 {
+		decoder := json.NewDecoder(file)
+		err = decoder.Decode(&tasks)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	var task = Task{
+		Id:          len(tasks) + 1,
+		Description: desc,
+		Status:      "todo",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	tasks = append(tasks, task)
+
+	file, err = os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	err = encoder.Encode(tasks)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
 }
