1d96f7464d7e1c17b0315682f8071ab8bfdfe332
mishgun <mvasilyev04@gmail.com>
2025-02-17 19:16:39 +0300

    implement update and decompose funcs

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"TaskTracker/enums"
 	"encoding/json"
+	"errors"
 	"log"
 	"os"
 	"slices"
@@ -30,7 +31,7 @@ func main() {
 	command := os.Args[1]
 	result := checkCommand(command)
 	if result == enums.Add {
-		description = getDescription()
+		description = getDescriptionForAdd()
 		addTask(tasks, filename, description)
 	} else if result == enums.Delete {
 		id, err := getId()
@@ -38,14 +39,28 @@ func main() {
 			log.Fatal("Need to write a number")
 		}
 		deleteTask(filename, id, tasks)
+	} else if result == enums.Update {
+		id, err := getId()
+		if err != nil {
+			log.Fatal(err)
+		}
+		description = getDescriptionForUpdate()
+		updateTask(id, tasks, filename, description)
 	}
 }
 
-func getDescription() string {
+func getDescriptionForAdd() string {
 	if len(os.Args) < 3 {
 		log.Fatal("You must write a task description")
 	}
 	return os.Args[2]
+}
+
+func getDescriptionForUpdate() string {
+	if len(os.Args) < 4 {
+		log.Fatal("You must write a task description")
+	}
+	return os.Args[3]
 }
 
 func getId() (int, error) {
@@ -94,8 +109,25 @@ func checkCommandList() int {
 	return result
 }
 
-func addTask(tasks []Task, filename, desc string) {
-	file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDONLY, 0644)
+func fileExist(filename string) bool {
+	_, err := os.Stat(filename)
+	if err == nil {
+		return true
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+	return false
+}
+
+func decode(tasks *[]Task, filename string) {
+	var flag int
+	if fileExist(filename) {
+		flag = os.O_CREATE | os.O_RDONLY
+	} else {
+		flag = os.O_RDONLY
+	}
+	file, err := os.OpenFile(filename, flag, 0644)
 	if err != nil && err.Error() != "EOF" {
 		log.Fatal(err)
 	}
@@ -107,11 +139,30 @@ func addTask(tasks []Task, filename, desc string) {
 	}
 	if info.Size() > 0 {
 		decoder := json.NewDecoder(file)
-		err = decoder.Decode(&tasks)
+		err = decoder.Decode(tasks)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
+}
+
+func encode(tasks []Task, filename string) {
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	err = encoder.Encode(tasks)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func addTask(tasks []Task, filename, desc string) {
+	decode(&tasks, filename)
 	var task = Task{
 		Id:          len(tasks) + 1,
 		Description: desc,
@@ -121,44 +172,20 @@ func addTask(tasks []Task, filename, desc string) {
 	}
 
 	tasks = append(tasks, task)
-	file, err = os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-	err = encoder.Encode(tasks)
-	if err != nil {
-		log.Fatal(err)
-	}
+	encode(tasks, filename)
 }
 
 func changeId(tasks []Task) []Task {
 	for i := 0; i < len(tasks); i++ {
-		tasks[i].Id -= 1
+		if tasks[i].Id != 1 {
+			tasks[i].Id -= 1
+		}
 	}
 	return tasks
 }
 
 func deleteTask(filename string, id int, tasks []Task) {
-	file, err := os.OpenFile(filename, os.O_RDONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	sizeFile, err := os.Stat(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if sizeFile.Size() > 0 {
-		decoder := json.NewDecoder(file)
-		err = decoder.Decode(&tasks)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+	decode(&tasks, filename)
 	var i int
 	for i = 0; i < len(tasks); i++ {
 		if tasks[i].Id == id {
@@ -167,15 +194,16 @@ func deleteTask(filename string, id int, tasks []Task) {
 	}
 	tasks = slices.Delete(tasks, i, i+1)
 	tasks = changeId(tasks)
-	file, err = os.OpenFile(filename, os.O_WRONLY|os.O_TRUNC, 0644)
-	if err != nil {
-		log.Fatal(err)
+	encode(tasks, filename)
+}
+
+func updateTask(id int, tasks []Task, filename, description string) {
+	decode(&tasks, filename)
+	for i := 0; i < len(tasks); i++ {
+		if tasks[i].Id == id {
+			tasks[i].Description = description
+		}
 	}
-	defer file.Close()
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-	err = encoder.Encode(tasks)
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	encode(tasks, filename)
 }
